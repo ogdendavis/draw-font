8ab98ced7d49f80f945e39dcd2cb9aaa28c18396
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-11-05 15:15:21 -0500

    App generates random generic CSS font values

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ class Letter extends React.Component {
     this.state = {
       letter: '',
     }
+    this.pickFontFamily = this.pickFontFamily.bind(this);
+    this.pickFontSize = this.pickFontSize.bind(this);
+    this.pickFontStyle = this.pickFontStyle.bind(this);
+    this.pickFontWeight = this.pickFontWeight.bind(this);
+    this.pickColor = this.pickColor.bind(this);
   }
 
   componentDidMount() {
@@ -14,14 +19,58 @@ class Letter extends React.Component {
     // level domain to select which letter they want to be drawn
     // (so drawfont.com/j would draw a j).
     if (this.state.letter === '') {
-      const newLetter = 'abcdefghijklmnopqrstuvwxyz'[Math.floor(Math.random() * 26)];
+      const newLetter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[Math.floor(Math.random() * 52)];
       this.setState( { letter: newLetter } )
     }
   }
 
+  pickFontFamily() {
+    const genericFamilies = ['serif', 'sans-serif', 'monospace', 'cursive', 'fantasy', 'system-ui'];
+    return genericFamilies[Math.floor(Math.random() * genericFamilies.length)];
+  }
+
+  pickFontSize() {
+    const maxHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    const pickedHeight = Math.floor(Math.random() * (maxHeight - 20));
+    return `${pickedHeight}px`;
+  }
+
+  pickFontStyle() {
+    const styles = ['normal', 'italic', 'normal', 'oblique']
+    const pickedStyle = styles[Math.floor(Math.random() * styles.length)];
+    if (pickedStyle === 'oblique') {
+      const degrees = Math.floor(Math.random() * 51) - 20; //range -20 to +30
+      return `${pickedStyle} ${degrees}deg`;
+    }
+    return pickedStyle;
+  }
+
+  pickFontWeight() {
+    return Math.floor(Math.random() * 1000) + 1;
+  }
+
+  pickLineHeight() {
+    return (Math.floor(Math.random() * 7) + 1) / 2;
+  }
+
+  pickColor(hex) {
+    hex += '0123456789abcdef'[Math.floor(Math.random() * 16)];
+    return hex.length === 6 ? hex : this.pickColor(hex);
+  }
+
   render() {
+
+    const style = {
+      color: `#${this.pickColor('')}`,
+      fontFamily: this.pickFontFamily(),
+      fontSize: this.pickFontSize(),
+      fontStyle: this.pickFontStyle(),
+      fontWeight: this.pickFontWeight(),
+      lineHeight: this.pickLineHeight(),
+    }
+
     return (
-      <p>{this.state.letter}</p>
+      <p style = {style}>{this.state.letter}</p>
     );
   }
 }
