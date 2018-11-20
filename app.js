b0d6a059f5672f854e067b5aaaa5ea794c15bd8f
Lucas Ogden-Davis <lucas@ogdendavis.com>
2018-11-20 17:03:13 -0500

    Restore newFont so random letter is generated

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ class Text extends React.Component {
     this.pickShadow = this.pickShadow.bind(this);
     this.setStyle = this.setStyle.bind(this);
     this.newLetter = this.newLetter.bind(this);
+    this.newFont = this.newFont.bind(this);
   }
 
   componentDidMount() {
@@ -245,6 +246,13 @@ class Text extends React.Component {
     this.setState( { letter: newLetter } )
   }
 
+  newFont() {
+    if (this.state.lorem === false) {
+      this.newLetter();
+    }
+    this.setStyle();
+  }
+
   render() {
     const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
@@ -255,7 +263,7 @@ class Text extends React.Component {
       <div className = "container">
         <div className = "buttonContainer">
           <LoremButton loremOn = {this.state.lorem} toggle = {this.toggleLorem} />
-          <button className = "newFontButton" onClick = {this.setStyle}>
+          <button className = "newFontButton" onClick = {this.newFont}>
             New font
           </button>
         </div>
